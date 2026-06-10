26c0544bd6bc1f5c50043b8c37d2ae4e4a021481
Raza Ali Murtaza <razaalymurtaza@gmail.com>
2026-06-10 21:36:13 +0500

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,9 +1,13 @@
-function Contact() {
+function Contact() 
+{
+    
     return (
+        
         <section id="contact" style={{ padding: '50px 20px' }}>
             <h2>Contact Me</h2>
             <p>This is the Contact section.</p>
         </section>
+        
     );
 }
 
